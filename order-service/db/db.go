2baf2e71df565f56818838072d262e51407c5138
Sayemm <sayemseer@gmail.com>
2025-05-30 01:08:59 -0600

    order-service: refactor

--- order-service/db/db.go
+++ order-service/db/db.go
@@ -11,7 +11,7 @@ var DB *sqlx.DB
 
 func InitDB() {
 	var err error
-	DB, err = sqlx.Connect("mysql", "root:password@tcp(order-db:3306)/orderdb")
+	DB, err = sqlx.Connect("mysql", "root:password@tcp(order-db:3306)/orderdb?parseTime=true")
 
 	if err != nil {
 		log.Fatalln("Database connection failedd: ", err)
